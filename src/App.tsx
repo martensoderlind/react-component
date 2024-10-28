e78e93041b6ae3edf55f47897eb4804bc34fed46
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-10-28 10:48:36 +0100

    api call with useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { WeatherCard } from "./weather-card/weatherCard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+const API_KEY = "194e08bef0674fc4a1685417242810";
+const location = "Stockholm";
 function App() {
   return (
     <>
-      <WeatherCard />
+      <QueryClientProvider client={queryClient}>
+        <WeatherCard apiKey={API_KEY} location={location} />
+      </QueryClientProvider>
     </>
   );
 }
--- src/weather-card/weatherCard.tsx
+++ src/weather-card/weatherCard.tsx
@@ -1,14 +1,38 @@
-export function WeatherCard() {
+import { useQuery } from "@tanstack/react-query";
+
+type WeatherCardProps = {
+  apiKey: string;
+  location: string;
+};
+
+export function WeatherCard({ apiKey, location }: WeatherCardProps) {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["weather"],
+    queryFn: async () => {
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`
+      );
+      if (!response.ok) {
+        throw new Error("Could not fetch people from api");
+      }
+      console.log(response);
+      return response.json();
+    },
+  });
+  if (isLoading) return <div>Laddar...</div>;
+
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
     <div className="bg-gray-300 flex flex-row w-48 m-2 rounded-md shadow-md">
       <div className="p-2">
-        <h3>molnigt</h3>
-        <img src="src\assets\sun.svg" alt="weather" />
+        <h3 className="">{data.current.condition.text}</h3>
+        <img src={data.current.condition.icon} alt="weather" />
       </div>
       <div className="border-l p-2">
-        <h2>Stockholm</h2>
-        <h3>dag</h3>
-        <h1>temp</h1>
+        <h2>{data.location.name} </h2>
+        <h3>{data.location.localtime}</h3>
+        <h1>{data.current.temp_c}</h1>
       </div>
     </div>
   );
