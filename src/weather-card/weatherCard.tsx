349c6cd8c455790c777d0fb692dbdbf49b4e2a7e
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-10-28 11:48:32 +0100

    some styling and new function for the fetch call

--- src/weather-card/weatherCard.tsx
+++ src/weather-card/weatherCard.tsx
@@ -5,9 +5,9 @@ type WeatherCardProps = {
   location: string;
 };
 
-export function WeatherCard({ apiKey, location }: WeatherCardProps) {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["weather"],
+const FetchWeatherData = (apiKey: string, location: string) => {
+  return useQuery({
+    queryKey: ["weather", location],
     queryFn: async () => {
       const response = await fetch(
         `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`
@@ -15,24 +15,38 @@ export function WeatherCard({ apiKey, location }: WeatherCardProps) {
       if (!response.ok) {
         throw new Error("Could not fetch people from api");
       }
-      console.log(response);
       return response.json();
     },
   });
-  if (isLoading) return <div>Laddar...</div>;
+};
 
-  if (error) return <div>Error: {error.message}</div>;
+export function WeatherCard({ apiKey, location }: WeatherCardProps) {
+  const { data, isLoading, error } = FetchWeatherData(apiKey, location);
+
+  if (isLoading)
+    return (
+      <div className="bg-gray-300 w-52 h-24 rounded-md shadow-md text-center">
+        Loading...
+      </div>
+    );
+
+  if (error)
+    return (
+      <div className="bg-gray-300 w-52 h-24 rounded-md shadow-md text-center">
+        Error: {error.message}
+      </div>
+    );
 
   return (
-    <div className="bg-gray-300 flex flex-row w-48 m-2 rounded-md shadow-md">
+    <div className="bg-slate-400 flex flex-row w-60 h-24 m-2 rounded-md shadow-md">
       <div className="p-2">
-        <h3 className="">{data.current.condition.text}</h3>
+        <h3 className="font-medium text-lg">{data.current.condition.text}</h3>
         <img src={data.current.condition.icon} alt="weather" />
       </div>
       <div className="border-l p-2">
-        <h2>{data.location.name} </h2>
-        <h3>{data.location.localtime}</h3>
-        <h1>{data.current.temp_c}</h1>
+        <h2 className="font-bold text-lg ">{data.location.name} </h2>
+        <h3 className="font-light text-xs">{data.location.localtime}</h3>
+        <h1 className="font-normal text-lg py-1">{data.current.temp_c} °C</h1>
       </div>
     </div>
   );
